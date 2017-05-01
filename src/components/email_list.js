af81bba97bcda987a87967ad8bfeef0179cc4e3c
josekim <joseph.kim85@gmail.com>
2017-05-01 17:53:41 -0500

    move files to component directory and update files

--- src/components/email_list.js
+++ src/components/email_list.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import RenderList from './render_list'
-import emails from "./Data"
+import emails from "../Data"
 import {SHOWALL,UNREAD,READ,SEARCHPROMPT} from './search_bar';
 
 
